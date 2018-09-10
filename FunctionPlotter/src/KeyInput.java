08b01b70567dfb648c9d0e41b4929eff83299233
IgorFranovic <igorfranovic1998@gmail.com>
2018-09-10 23:04:06 +0200

    Added a buttonListener, keyInput class, and extra gui components

---
+++ FunctionPlotter/src/KeyInput.java
@@ -0,0 +1,21 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter {
+	
+	public void keyPressed(KeyEvent event) {
+		int key = event.getKeyCode();
+		
+		if(key == KeyEvent.VK_ESCAPE) System.exit(1);
+		
+		switch(key) {
+			
+			case KeyEvent.VK_ESCAPE : System.exit(1);
+				break;
+			case KeyEvent.VK_ENTER : Window.JButtonDraw.doClick();
+				break;
+		}
+	}
+
+		
+}
--- FunctionPlotter/src/Window.java
+++ FunctionPlotter/src/Window.java
@@ -1,16 +1,22 @@
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Window extends JFrame {
 	// code for the GUI of the program
 	
 	
 	public static JTextArea JTextAreaFunction;
-	public static JTextArea JTextAreaMin;
-	public static JTextArea JTextAreaMax;
+	public static JTextArea JTextAreaMinX;
+	public static JTextArea JTextAreaMaxX;
+	public static JTextArea JTextAreaMinY;
+	public static JTextArea JTextAreaMaxY;
+	
 	
 	public static JButton JButtonDraw;
 	
@@ -21,6 +27,7 @@ public class Window extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setLayout(null);
+//		this.addKeyListener(new KeyInput());
 		
 		
 		
@@ -29,24 +36,59 @@ public class Window extends JFrame {
 		JTextAreaFunction.setBounds(30, 50, 120, 40);
 		this.add(JTextAreaFunction);
 		
-		//JTextAreaMin
-		JTextAreaMin = new JTextArea();
-		JTextAreaMin.setBounds(30, 200, 50, 40);
-		this.add(JTextAreaMin);
+		//JTextAreaMinX
+		JTextAreaMinX = new JTextArea();
+		JTextAreaMinX.setBounds(30, 200, 50, 40);
+		this.add(JTextAreaMinX);
 		
-		//JTextAreaMax
-		JTextAreaMax = new JTextArea();
-		JTextAreaMax.setBounds(100, 200, 50, 40);
-		this.add(JTextAreaMax);
+		//JTextAreaMaxX
+		JTextAreaMaxX = new JTextArea();
+		JTextAreaMaxX.setBounds(100, 200, 50, 40);
+		this.add(JTextAreaMaxX);
+		
+		//JTextAreaMinY
+		JTextAreaMinY = new JTextArea();
+		JTextAreaMinY.setBounds(30, 300, 50, 40);
+		this.add(JTextAreaMinY);
+
+		//JTextAreaMaxY
+		JTextAreaMaxY = new JTextArea();
+		JTextAreaMaxY.setBounds(100, 300, 50, 40);
+		this.add(JTextAreaMaxY);
 		
 		//JButtonDraw
 		JButtonDraw = new JButton("Draw");
 		JButtonDraw.setBounds(30, 120, 120, 40);
+		JButtonDraw.setActionCommand("JButtonDraw");
 		this.add(JButtonDraw);
 		
 		
 		
+		
 		this.setVisible(true);
+	}
+	
+	public static class ButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			String actionCommand = event.getActionCommand();
+			
+			switch(actionCommand) {
+				
+				case "JButtonDraw" : {
+					
+					//add code for drawing the function here
+					
+					
+					
+					
+					
+				} break;
+			}
+			
+		}
+		
 	}
 	
 }
