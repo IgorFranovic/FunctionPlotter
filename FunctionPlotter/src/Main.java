0eb2dabdbe1f886ae0abe39f626ad2dbb4ccc63c
IgorFranovic <igorfranovic1998@gmail.com>
2018-09-10 21:04:08 +0200

    Added code to the Window class.

--- FunctionPlotter/src/Main.java
+++ FunctionPlotter/src/Main.java
@@ -1,9 +1,11 @@
 
-public class Test {
+public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Window window = new Window();
+	
+		
+		Window window = new Window("Function Plotter", 1200, 800);
+		
 	}
 
 }
--- FunctionPlotter/src/Window.java
+++ FunctionPlotter/src/Window.java
@@ -1,8 +1,19 @@
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Window extends JFrame {
 	// code for the GUI of the program
 	
-	// neki komentar
+	public Window(String windowName, int WIDTH, int HEIGHT) {
+		
+		this.setSize(new Dimension(WIDTH, HEIGHT));
+		this.setTitle(windowName);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		
+		
+	}
 	
 }
